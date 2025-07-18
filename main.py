3b46040a2a17ed4ba19c6d587d55c7cdac1c49d7
beromerrr <bratmeow378@gmail.com>
2025-07-18 16:20:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 videos = [
     "videos/VARG1.mp4",
@@ -8,14 +9,14 @@ videos = [
     "videos/VARG4.mp4"
 ]
 
-TRIGGER_WORD = "Варг"
+TRIGGER_WORD = "варг"
 
-def handle_message(update, context):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.lower()
     if TRIGGER_WORD in text:
         video_path = random.choice(videos)
         with open(video_path, 'rb') as video_file:
-            context.bot.send_video(
+            await context.bot.send_video(
                 chat_id=update.effective_chat.id,
                 video=video_file,
                 reply_to_message_id=update.message.message_id
@@ -23,14 +24,12 @@ def handle_message(update, context):
 
 def main():
     TOKEN = "8198057099:AAEhMiejp6XQj0s5NnEiPXoy2HQp3deYM_w"
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+    app = ApplicationBuilder().token(TOKEN).build()
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("Бот запущен")
-    updater.start_polling()
-    updater.idle()
+    app.run_polling()
 
 if __name__ == '__main__':
     main()

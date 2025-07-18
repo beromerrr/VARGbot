28f4ff32b0f8a6caff10150b12d1d802bd79c061
beromerrr <bratmeow378@gmail.com>
2025-07-18 19:35:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
-
-@app.route(f"/{os.environ.get('BOT_TOKEN')}", methods=["POST"])
-def webhook():
-    print("Webhook received!")
-    return "OK"
 
 @app.route("/", methods=["GET"])
 def root():
     return "Bot is alive!"
 
+@app.route(f"/{os.environ.get('BOT_TOKEN')}", methods=["POST"])
+def webhook():
+    print("Webhook received!")
+    return "ok"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
